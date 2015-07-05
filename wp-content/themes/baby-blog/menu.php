fc73e412b165cd3aa34c9d3fcea33bf66e8d55b9
superfrigo <montet.thomas@gmail.com>
2015-07-05 11:53:34 +0200

    remove arrows

--- wp-content/themes/baby-blog/menu.php
+++ wp-content/themes/baby-blog/menu.php
@@ -1,6 +1,6 @@
 <ul id="menu">
 				<li><a href="/" title="Homepage"><img src="<?php bloginfo('template_url'); ?>/img/skin/btn-home.png"/></a></li>
-				<li><a href="/comic/{{comics.first.number}}" title="First comic"><img src="<?php bloginfo('template_url'); ?>/img/skin/btn-first.png"/></a></li>
+				<li><a href="#" title="First comic"><img src="<?php bloginfo('template_url'); ?>/img/skin/btn-first.png"/></a></li>
 				<li>
 					<a href="<?php echo get_previous_post_url(); ?>" title="Previous"><img src="<?php bloginfo('template_url'); ?>/img/skin/btn-prev.png"/></a>
 				</li>
@@ -8,5 +8,5 @@
 				<li>
 					<a href="<?php echo get_next_post_url(); ?>" title="Next"><img src="<?php bloginfo('template_url'); ?>/img/skin/btn-next.png"/></a>
 				</li>
-				<li><a href="/comic/{{comics.last.number}}" title="Last"><img src="<?php bloginfo('template_url'); ?>/img/skin/btn-last.png"/></a></li>
+				<li><a href="#" title="Last"><img src="<?php bloginfo('template_url'); ?>/img/skin/btn-last.png"/></a></li>
 			</ul>
