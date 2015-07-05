8d5da2dc566f4db2994db2e50115d850e342b2f9
superfrigo <montet.thomas@gmail.com>
2015-07-05 11:31:55 +0200

    much favicons

--- wp-content/themes/baby-blog/header.php
+++ wp-content/themes/baby-blog/header.php
@@ -8,6 +8,26 @@
 			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
 		<![endif]-->
 		<base href="<?php bloginfo('url'); ?>">
+		<link rel="apple-touch-icon-precomposed" sizes="57x57" href="<?php bloginfo('url'); ?>/apple-touch-icon-57x57.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="114x114" href="<?php bloginfo('url'); ?>/apple-touch-icon-114x114.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="72x72" href="<?php bloginfo('url'); ?>/apple-touch-icon-72x72.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="144x144" href="<?php bloginfo('url'); ?>/apple-touch-icon-144x144.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="60x60" href="<?php bloginfo('url'); ?>/apple-touch-icon-60x60.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="120x120" href="<?php bloginfo('url'); ?>/apple-touch-icon-120x120.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="76x76" href="<?php bloginfo('url'); ?>/apple-touch-icon-76x76.png" />
+		<link rel="apple-touch-icon-precomposed" sizes="152x152" href="<?php bloginfo('url'); ?>/apple-touch-icon-152x152.png" />
+		<link rel="icon" type="image/png" href="<?php bloginfo('url'); ?>/favicon-196x196.png" sizes="196x196" />
+		<link rel="icon" type="image/png" href="<?php bloginfo('url'); ?>/favicon-96x96.png" sizes="96x96" />
+		<link rel="icon" type="image/png" href="<?php bloginfo('url'); ?>/favicon-32x32.png" sizes="32x32" />
+		<link rel="icon" type="image/png" href="<?php bloginfo('url'); ?>/favicon-16x16.png" sizes="16x16" />
+		<link rel="icon" type="image/png" href="<?php bloginfo('url'); ?>/favicon-128.png" sizes="128x128" />
+		<meta name="application-name" content="Baby Boredom"/>
+		<meta name="msapplication-TileColor" content="#FFFFFF" />
+		<meta name="msapplication-TileImage" content="<?php bloginfo('url'); ?>/mstile-144x144.png" />
+		<meta name="msapplication-square70x70logo" content="<?php bloginfo('url'); ?>/mstile-70x70.png" />
+		<meta name="msapplication-square150x150logo" content="<?php bloginfo('url'); ?>/mstile-150x150.png" />
+		<meta name="msapplication-wide310x150logo" content="<?php bloginfo('url'); ?>/mstile-310x150.png" />
+		<meta name="msapplication-square310x310logo" content="<?php bloginfo('url'); ?>/mstile-310x310.png" />
 		<script>(function(d, s, id) {
 		  var js, fjs = d.getElementsByTagName(s)[0];
 		  if (d.getElementById(id)) return;
